d9d86c1fff595786ca5a2f141c7fe74cab701d62
ArsenFabulous <104206419+ArsenFabulous@users.noreply.github.com>
2022-09-08 16:09:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+var myHeaders = new Headers();
+myHeaders.append("apikey", "75r16726IFyt9K5HSCuBZ9I516KKlzMz");
 
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=USD%2CEUR&base=RUB", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
