dbd48cadc7c57b376af925c0fd8f6c379cd6ca00
ArsenFabulous <104206419+ArsenFabulous@users.noreply.github.com>
2022-09-09 14:45:05 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// API exchange rates
+/*
 var myHeaders = new Headers();
 myHeaders.append("apikey", "75r16726IFyt9K5HSCuBZ9I516KKlzMz");
 
@@ -11,3 +13,19 @@ fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=USD%2CEUR&base
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
+*/
+
+let regex = /[ ]/g;
+
+let amount = document.querySelector('.amount');
+
+amount.oninput = function () {
+  this.value = this.value.replace(regex, '');    //попробовать сделать общую функцию
+}
+
+let recalculation = document.querySelector('.recalculation');
+
+recalculation.oninput = function () {
+  this.value = this.value.replace(regex, '');
+}
+
