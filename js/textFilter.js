a71afb536d8c9997150d6bc00bfd3a6524d12331
ArsenFabulous <104206419+ArsenFabulous@users.noreply.github.com>
2022-09-19 22:52:12 +0300

    Update textFilter.js

--- js/textFilter.js
+++ js/textFilter.js
@@ -1,4 +1,4 @@
-let regex = /[^0-9.]/g;
+const regex = /[^0-9.]/g;
 
 let amount = document.querySelector('.amount');
 
@@ -11,3 +11,5 @@ let recalculation = document.querySelector('.recalculation');
 recalculation.oninput = function () {
   this.value = this.value.replace(regex, '');
 }
+
+// добавить через регулярное выражение фильтр на ноль в начале и одну точку, которая переставляется при вторичном вводе
