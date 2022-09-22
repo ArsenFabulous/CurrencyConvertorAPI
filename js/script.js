6aabef7ec8adcb18e5841c058fef5cf7c5852dfa
ArsenFabulous <104206419+ArsenFabulous@users.noreply.github.com>
2022-09-22 16:19:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // API exchange rates
+
 /*
 var myHeaders = new Headers();
 myHeaders.append("apikey", "75r16726IFyt9K5HSCuBZ9I516KKlzMz");
@@ -8,21 +9,33 @@ var requestOptions = {
   headers: myHeaders
 };
 fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=USD%2CEUR&base=RUB", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
+.then((response) => {
+  return response.json(); // я так думаю он вренул в дату resonse
+})
+.then((data) => {
+  
+  console.log(data);
+   console.log(data.date);
+})
   .catch(error => console.log('error', error));
+
+
+//рассмотреть подробнее как работают промисы
 */
 
-const currencyArray = document.querySelectorAll('.currency-name');
-
 let myCurrencyIndex = 0; //ключ для переключения валют (У меня есть)
-let switcherCurrency  //при переключении через switcher
+let switcherCurrency  //промежуточный ключ
 let otherCurrencyIndex = 1; //ключ для преключения валют (Хочу купить)
 
-//У меня есть
+//У меня есть (левая часть)
 const rub = document.querySelector('.rub');
 const usd = document.querySelector('.usd');
 const eur = document.querySelector('.eur');
+
+//Хочу купить (правая часть)
+const oRub = document.querySelector('.o-rub');
+const oUsd = document.querySelector('.o-usd');
+const oEur = document.querySelector('.o-eur');
 
 //функция для передачи ключа
 function indexation(index, whichIndex, classTo) {
@@ -44,11 +57,6 @@ eur.addEventListener("click", function () {
   return myCurrencyIndex = indexation(2, myCurrencyIndex, eur);
 })
 
-//Хочу купить
-const oRub = document.querySelector('.o-rub');
-const oUsd = document.querySelector('.o-usd');
-const oEur = document.querySelector('.o-eur');
-
 //передача индекса по клику
 oRub.addEventListener("click", function () {
   localClassSwitch();
@@ -63,12 +71,25 @@ oEur.addEventListener("click", function () {
   return otherCurrencyIndex = indexation(2, otherCurrencyIndex, oEur);
 })
 
-const switcher = document.getElementById('Capa_1');
+//функция для изменения отображения выбранной валюты в меню
+let leftCurrencyNames = document.querySelectorAll('.currency-name');
+let rightCurrencyNames = document.querySelectorAll('.o-currency-name');
+function localClassSwitch(writeAnythingToLeftSide) {
+  writeAnythingToLeftSide ?
+    leftCurrencyNames.forEach(n => n.classList.remove('active'))
+    :
+    rightCurrencyNames.forEach(n => n.classList.remove('active'))
+  event.target.classList.add('active');
+}
+
+const switcher = document.getElementById('Capa_1'); //svg стрелки в середине
 
 switcher.addEventListener("click", function () {
   switchCurrencyIndex();
+  switchCurrencyClass();
 });
 
+//свитчит индексы 
 function switchCurrencyIndex() {
   switcherCurrency = myCurrencyIndex;
   myCurrencyIndex = otherCurrencyIndex;
@@ -76,17 +97,33 @@ function switchCurrencyIndex() {
   console.dir([myCurrencyIndex, otherCurrencyIndex, switcherCurrency]);
 };
 
+//свитчит класс по нажатию на svg стрелки
 function switchCurrencyClass() {
-  if (myCurrencyIndex === otherCurrencyIndex) { return };
-
-}
-//функция для изменения отображения в меню
-function localClassSwitch(left) {
-  left ?
-    document.querySelectorAll('.currency-name')
-      .forEach(n => n.classList.remove('active'))
-    :
-    document.querySelectorAll('.o-currency-name')
-      .forEach(n => n.classList.remove('active'))
-  event.target.classList.add('active');
+  if (myCurrencyIndex === otherCurrencyIndex) { return }
+  else {
+    leftCurrencyNames.forEach(n => n.classList.remove('active'));
+    rightCurrencyNames.forEach(n => n.classList.remove('active'));
+    switch (myCurrencyIndex) {
+      case 0:
+        rub.classList.add('active');
+        break;
+      case 1:
+        usd.classList.add('active');
+        break;
+      case 2:
+        eur.classList.add('active');
+        break;
+    };
+    switch (otherCurrencyIndex) {
+      case 0:
+        oRub.classList.add('active');
+        break;
+      case 1:
+        oUsd.classList.add('active');
+        break;
+      case 2:
+        oEur.classList.add('active');
+        break;
+    }
+  }
 }
